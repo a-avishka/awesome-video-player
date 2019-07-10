83cc2074ece7c4f76b6e19374e3fa91818578c36
a-avishka <a.avishka99@gmail.com>
2019-07-11 02:55:39 +0530

    Commit 2: Version 1.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,46 +1,72 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 
+
 public class Main extends Application {
 
 
     private static Stage stage;
 
+
+
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         primaryStage.setTitle("VideoPlayer");
 
-        Scene scene = new Scene(root,600,337.5);
+        Scene scene = new Scene(root);
+
+
         scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+
             boolean fullScr = false;
+
 
             @Override
             public void handle(MouseEvent event) {
-                if(event.getClickCount() == 2){
+                if (event.getClickCount() == 2) {
                     fullScr = (!fullScr);
                     primaryStage.setFullScreen(fullScr);
+
+
                 }
+
             }
+
+
         });
 
         primaryStage.setScene(scene);
         primaryStage.show();
 
         stage = primaryStage;
+
+
+        primaryStage.addEventFilter(KeyEvent.KEY_PRESSED, k -> {
+            if ( k.getCode() == KeyCode.SPACE){
+                k.consume();
+            }
+        });
+
     }
 
 
@@ -52,8 +78,6 @@ public class Main extends Application {
     public static Stage getStage() {
         return stage;
     }
-
-
 
 
 
